4ed20a9e257a20f3c9c265b03c3ed63f676d609a
Yuji Yashima - Lab 04 Remoto <>
2021-03-02 22:07:03 -0300

    Aula 02/03/2021

---
+++ src/java/br/com/projetoempresa/util/scriptBanco.sql
@@ -0,0 +1,20 @@
+create table PESSOA(
+
+	idpessoa serial not null,
+	nomepessoa varchar(50),
+	telefonepessoa varchar(11),
+	constraint pk_pessoa primary key(idpessoa)
+
+);
+
+
+create table PESSOAJURIDICA(
+
+	idpessoajuridica serial not null,
+	cnpjpessoajuridica varchar(14),
+	iepessoajuridica varchar(10),
+	idpessoa integer,
+	constraint pk_pessoajuridica primary key(idpessoajuridica),
+	constraint fk_pj_pessoa foreign key(idpessoa) references pessoa(idpessoa)
+);
+
